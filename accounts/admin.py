99e54ea5a639151d6c5f4db67cbb7712390decf5
Drills-hub <sdi6566@gmail.com>
2024-09-26 12:23:12 +0900

    feat:"community/models"
    1.커뮤니티 모델 구현
    2.관리자 페이지(백오피스)에 카테고리,댓글,게시글 등록

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
+from community.models import Category, Community, Comment
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Community)
+admin.site.register(Comment)
