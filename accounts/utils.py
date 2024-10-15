f2eb1f0bb03cbed14c4dbc875e6726a4ead71353
김지윤 <jiyoonsworld@gimjiyun-ui-MacBookAir.local>
2024-10-15 14:35:45 +0900

    "이메일 제목 내용 보완"

--- accounts/utils.py
+++ accounts/utils.py
@@ -11,8 +11,8 @@ def send_verification_email(user):
     uid = encode_uid(user)
     activation_link = f"http://127.0.0.1:8000{reverse('verify-email', kwargs={'uidb64': uid, 'token': token})}"
 
-    subject = "이메일 인증"
-    message = f"아래 링크를 클릭하여 이메일을 인증하세요:\n{activation_link}"
+    subject = "sulmeulliae 이메일 인증"
+    message = f"가입을 환영합니다. 아래 링크를 클릭하여 이메일을 인증하세요:\n{activation_link}"
     
     send_mail(subject, message, 'sulmeulliae@gamil.com', [user.email])
 
