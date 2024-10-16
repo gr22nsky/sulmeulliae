3186bf0a2e9a798c1b56e3d3ca7d4253c65a1d06
gr22nsky <gr2nsky@gmail.com>
2024-10-16 11:58:22 +0900

    signup_fix

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -18,7 +18,10 @@ class UserSerializer(serializers.ModelSerializer):
         return value
 
     def create(self, validated_data):
+        profile_image = validated_data.pop('profile_image', None)
         user = User.objects.create_user(**validated_data)
+        if not profile_image:
+            user.profile_image = 'profile_images/default_profile.jpeg'
         user.is_email_verified = False
         user.save()
         send_verification_email(user)  # 이메일 인증 메일 전송
