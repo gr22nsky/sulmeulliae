572144c6ac6cf7a77ca5d0e3a8a9b17a3e3b340b
gr22nsky <gr2nsky@gmail.com>
2024-10-21 19:49:02 +0900

    admin_chat

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from.models import Chatroom
+from.models import ChatRoom
 
 # Register your models here.
-admin.site.register(Chatroom)
+admin.site.register(ChatRoom)
